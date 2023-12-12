2e38fa107384bb8b99976239a01257057667ad47
ReCaree <86810014+ReCaree@users.noreply.github.com>
2023-12-12 18:14:12 +0700

    Update checker.py

--- checker.py
+++ checker.py
@@ -5,8 +5,7 @@ from threading import Thread
 
 # https://github.com/iw4p/proxy-scraper/blob/master/proxyChecker.py
 
-Console.print_logo()
-# Console.print_seperator()
+
 
 user_agents = []
 with open("user_agents.txt", "r") as f:
@@ -88,5 +87,8 @@ def check(file, timeout, method, site, verbose, random_user_agent):
 
     print(f"Found {len(valid_proxies)} valid proxies")
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    Console.print_logo()
+    # Console.print_seperator()
+    
     check("./proxy/http-test.txt", 100, "http", "https://google.com/", True, True)
